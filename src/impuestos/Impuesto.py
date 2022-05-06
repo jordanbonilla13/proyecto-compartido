a46f42f2daba455174e6dd64dcdf91f6164b348b
AlexHugo124 <alexis124as@gmail.com>
2022-05-06 11:51:42 +0200

    terminado todo

---
+++ src/impuestos/Impuesto.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import date
+
+@dataclass
+class Impuesto:
+    id: int
+    nombre: str
+    valor: float
+    fecha_inicio: date
+    fecha_fin: date =None
+
+    def __repr__ (self) ->str:
+        return f'IMPUESTO{self.id} {self.nombre} {self.valor}{self.fecha_inicio}{self.fecha_fin}'
